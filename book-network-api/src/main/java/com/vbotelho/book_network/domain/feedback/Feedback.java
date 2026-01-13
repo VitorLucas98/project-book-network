3ba6d1864f3d1ebf848968b3e4fa027890b2841c
Vitor Lucas <vitorluc.2013@gmail.com>
2026-01-13 08:56:37 -0300

    Implementation the feedback entity

---
+++ book-network-api/src/main/java/com/vbotelho/book_network/domain/feedback/Feedback.java
@@ -0,0 +1,23 @@
+package com.vbotelho.book_network.domain.feedback;
+
+import com.vbotelho.book_network.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Feedback extends BaseEntity {
+
+    @Column
+    private Double note;
+    private String comment;
+}
