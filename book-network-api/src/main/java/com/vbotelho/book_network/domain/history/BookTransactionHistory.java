d4b02eada3f0fb3766bbb0254aed9de54f88706b
Vitor Lucas <vitorluc.2013@gmail.com>
2026-01-13 09:27:23 -0300

    Implementation the transaction history entity

---
+++ book-network-api/src/main/java/com/vbotelho/book_network/domain/history/BookTransactionHistory.java
@@ -0,0 +1,21 @@
+package com.vbotelho.book_network.domain.history;
+
+import com.vbotelho.book_network.domain.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+
+    private boolean returned;
+    private boolean returnApproved;
+}
