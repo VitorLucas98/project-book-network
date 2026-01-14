e4c131791a169c13cf6744619554f8b4fc218979
Vitor Lucas <vitorluc.2013@gmail.com>
2026-01-14 18:49:34 -0300

    Implement the find all displayable books

--- book-network-api/src/main/java/com/vbotelho/book_network/controllers/BookController.java
+++ book-network-api/src/main/java/com/vbotelho/book_network/controllers/BookController.java
@@ -3,6 +3,7 @@ package com.vbotelho.book_network.controllers;
 import com.vbotelho.book_network.services.BookService;
 import com.vbotelho.book_network.services.dto.BookRequest;
 import com.vbotelho.book_network.services.dto.BookResponse;
+import com.vbotelho.book_network.services.dto.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,17 +26,21 @@ public class BookController {
     private final BookService service;
 
     @PostMapping
-    public ResponseEntity<Long> saveBook(
-            @Valid @RequestBody BookRequest request,
-            Authentication connectedUser
-    ) {
+    public ResponseEntity<Long> saveBook(@Valid @RequestBody BookRequest request,
+            Authentication connectedUser) {
         return ResponseEntity.ok(service.save(request, connectedUser));
     }
 
     @GetMapping("/{book-id}")
-    public ResponseEntity<BookResponse> findBookById(
-            @PathVariable("book-id") Long bookId
-    ) {
+    public ResponseEntity<BookResponse> findBookById(@PathVariable("book-id") Long bookId) {
         return ResponseEntity.ok(service.findById(bookId));
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooks(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser) {
+        return ResponseEntity.ok(service.findAllBooks(page, size, connectedUser));
     }
 }
--- book-network-api/src/main/java/com/vbotelho/book_network/services/BookService.java
+++ book-network-api/src/main/java/com/vbotelho/book_network/services/BookService.java
@@ -5,13 +5,20 @@ import com.vbotelho.book_network.domain.book.BookRepository;
 import com.vbotelho.book_network.domain.user.User;
 import com.vbotelho.book_network.services.dto.BookRequest;
 import com.vbotelho.book_network.services.dto.BookResponse;
+import com.vbotelho.book_network.services.dto.PageResponse;
 import com.vbotelho.book_network.services.mapper.BookMapper;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -32,5 +39,23 @@ public class BookService {
         return bookRepository.findById(bookId)
                 .map(BookMapper::toBookResponse)
                 .orElseThrow(() -> new EntityNotFoundException("No book found with ID:: " + bookId));
+    }
+
+    public PageResponse<BookResponse> findAllBooks(int page, int size, Authentication connectedUser) {
+        User user = ((User) connectedUser.getPrincipal());
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdDate").descending());
+        Page<Book> books = bookRepository.findAllDisplayableBooks(pageable, user.getId());
+        List<BookResponse> booksResponse = books.stream()
+                .map(BookMapper::toBookResponse)
+                .toList();
+        return new PageResponse<>(
+                booksResponse,
+                books.getNumber(),
+                books.getSize(),
+                books.getTotalElements(),
+                books.getTotalPages(),
+                books.isFirst(),
+                books.isLast()
+        );
     }
 }
